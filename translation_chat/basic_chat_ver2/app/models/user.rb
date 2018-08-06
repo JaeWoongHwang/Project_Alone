b269131c0bd750048671508fb966245b5e5a47b3
JaeWoongHwang <jwngh15@gmail.com>
2018-08-06 14:06:37 +0900

    Define User's columns

---
+++ translation_chat/basic_chat_ver2/app/models/user.rb
@@ -0,0 +1,14 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+  before_save {self.email = email.downcase}
+  VALID_EMAIL_REGEX = /\A([\w+\-]\.?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i   # 정규식을 이용해 이메일 형식을 지정
+  validates :nickname, presence: true, length: {maximum: 50}               # 이름과 이메일이 반드시 존재하고 50,255자를 넘지 않게하는 유효성검사
+  validates :email, presence: true, length: {maximum: 255},                # validates (존재를 확인), presence(오직 하나만 존재),
+  format: {with: VALID_EMAIL_REGEX}, uniqueness: {case_sensitive: false}   # uniqueness(이메일소문자)
+
+  has_many :posts
+end
---
+++ translation_chat/basic_chat_ver2/db/migrate/20180806043903_add_userattribute_to_users.rb
@@ -0,0 +1,9 @@
+class AddUserattributeToUsers < ActiveRecord::Migration[5.2]
+  def change
+    add_column :users, :nickname, :string
+    add_column :users, :age, :integer
+    add_column :users, :nationality, :string
+    add_column :users, :gender, :string
+    add_column :users, :admin, :boolean
+  end
+end
