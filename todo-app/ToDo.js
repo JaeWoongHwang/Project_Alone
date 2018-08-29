aea2f28e71430a8a1a61fe6ecca620fea5fec916
JaeWoongHwang <jwngh15@gmail.com>
2018-08-29 12:50:02 +0900

    Styling the To Do's Component

--- todo-app/ToDo.js
+++ todo-app/ToDo.js
@@ -9,13 +9,36 @@ export default class ToDo extends Component{
     isCompleted: false
   };
   render(){
-    const {isCompleted} = this.state;
+    const {isCompleted, isEditing} = this.state;
     return(
       <View style ={styles.container}>
-      <TouchableOpacity onPress={this._toggleComplete}>
-        <View style = {[styles.circle, isCompleted? styles.completedCircle : styles.uncompletedCircle]}/>
-      </TouchableOpacity>
-      <Text style = {styles.text}>Hello I am a To Do</Text>
+        <View style ={styles.column}>
+          <TouchableOpacity onPress={this._toggleComplete}>
+            <View style = {[styles.circle, isCompleted? styles.completedCircle : styles.uncompletedCircle]}/>
+          </TouchableOpacity>
+          <Text style = {[styles.text, isCompleted? styles.completedText : styles.uncompletedText]}>Hello I am a To Do</Text>
+        </View>
+          {isEditing ? (
+            <View style={styles.actions}>
+              <TouchableOpacity onPressOut={this._finishEditing}>
+                <View style={styles.actionContainer}>
+                  <Text style={styles.actionText}>✔</Text>
+                </View>
+              </TouchableOpacity>
+            </View>
+          ) : (<View style={styles.actions}>
+              <TouchableOpacity onPressOut={this._startEditing}>
+                <View style={styles.actionContainer}>
+                  <Text style={styles.actionText}>✎</Text>
+                </View>
+              </TouchableOpacity>
+              <TouchableOpacity>
+                <View style={styles.actionContainer}>
+                  <Text style={styles.actionText}>✘</Text>
+                </View>
+              </TouchableOpacity>
+            </View>
+          )}
       </View>
     );
   }
@@ -26,6 +49,16 @@ export default class ToDo extends Component{
       };
     });
   };
+  _startEditing = () => {
+    this.setState({
+      isEditing: true
+    });
+  };
+  _finishEditing = () =>{
+    this.setState({
+      isEditing: false
+    });
+  };
 }
 
 const styles = StyleSheet.create({
@@ -34,12 +67,20 @@ const styles = StyleSheet.create({
     borderBottomColor: "#bbb",
     borderBottomWidth: StyleSheet.hairlineWidth,
     flexDirection: "row",
-    alignItems: "center"
+    alignItems: "center",
+    justifyContent: "space-between"
   },
   text:{
     fontWeight: "600",
     fontSize: 20,
     marginVertical: 20
+  },
+  completedText:{
+    color: "#bbb",
+    textDecorationLine: "line-through"
+  },
+  uncompletedText:{
+    color: "#353535"
   },
   circle:{
     width: 30,
@@ -53,5 +94,21 @@ const styles = StyleSheet.create({
   },
   uncompletedCircle:{
     borderColor: "skyblue"
+  },
+  column:{
+    flexDirection: "row",
+    alignItems: "center",
+    width: width / 2,
+    justifyContent: "space-between"
+  },
+  actions:{
+    flexDirection: "row"
+  },
+  actionContainer:{
+    marginVertical: 10,
+    marginHorizontal: 10
+  },
+  actionText: {
+    fontSize: 30
   }
 });
