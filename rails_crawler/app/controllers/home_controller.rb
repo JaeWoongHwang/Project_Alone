d3b465e43cf0236ba7b440e6b2f203d1655182f9
JaeWoongHwang <jwngh15@gmail.com>
2019-04-15 19:02:41 +0900

    Commit before leaving work

--- rails_crawler/app/controllers/home_controller.rb
+++ rails_crawler/app/controllers/home_controller.rb
@@ -7,10 +7,17 @@ class HomeController < ApplicationController
     doc = Nokogiri::HTML(open("https://m.dhlottery.co.kr/gameResult.do?method=byWin&drwNo=853"))
     doc.css(".contents .bx_lotto_winnum").each do |element|
       @number << {
-        num:element.children("span:not('.plus')").text
-
+        num1:element.css('span:nth-child(1)').text,
+        num2:element.css('span:nth-child(2)').text,
+        num3:element.css('span:nth-child(3)').text,
+        num4:element.css('span:nth-child(4)').text,
+        num5:element.css('span:nth-child(5)').text,
+        num6:element.css('span:nth-child(6)').text,
+        num7:element.css('span:nth-child(8)').text
       }
     end
+
+    @number.split('')
   end
 
   def index
