d70137a6d707b6490eabda9ee54fedbbc04a1a95
JaeWoongHwang <jwngh15@gmail.com>
2019-04-16 19:14:47 +0900

    Commit before leaving work

--- rails_crawler/app/controllers/home_controller.rb
+++ rails_crawler/app/controllers/home_controller.rb
@@ -3,8 +3,10 @@ class HomeController < ApplicationController
   
   def crawler
     @number = []
-
+    @round = []
     doc = Nokogiri::HTML(open("https://m.dhlottery.co.kr/gameResult.do?method=byWin&drwNo=853"))
+    @round << {info:doc.css(".contents .wrap_select #dwrNoList option[type=selected]").text}
+
     doc.css(".contents .bx_lotto_winnum").each do |element|
       @number << {
         num1:element.css('span:nth-child(1)').text,
@@ -13,7 +15,7 @@ class HomeController < ApplicationController
         num4:element.css('span:nth-child(4)').text,
         num5:element.css('span:nth-child(5)').text,
         num6:element.css('span:nth-child(6)').text,
-        num7:element.css('span:nth-child(8)').text
+        bonusNum:element.css('span:nth-child(8)').text
       }
     end
 
